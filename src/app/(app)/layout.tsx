f74124ca5c30edc6c014db8b02fabfdb39aca020
SagnikGhosh001 <sagnikghosh904@gmail.com>
2025-02-27 01:17:52 +0530

    with typescrpt rules diable

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,13 +1,15 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css"; // Adjust path since it's now deeper
+// import "../globals.css"; // Adjust path since it's now deeper
 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export default function AppLayout({
